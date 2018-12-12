6e291c60f494bfc438ba70d18c4a48a572e56a2f
devanks <32042038+devanks@users.noreply.github.com>
2018-12-12 17:04:00 +0530

    Update admin.py

--- secretSanta/admin.py
+++ secretSanta/admin.py
@@ -10,6 +10,6 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', 'secret_santa_of']
+    list_display = ['email', 'username']
 
 admin.site.register(CustomUser, CustomUserAdmin)
