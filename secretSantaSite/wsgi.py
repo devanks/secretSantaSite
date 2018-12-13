09d370fe66392780355cda96fc3435846c44bc04
devanks <32042038+devanks@users.noreply.github.com>
2018-12-13 20:20:51 +0530

    Mobile set

--- secretSantaSite/wsgi.py
+++ secretSantaSite/wsgi.py
@@ -10,10 +10,13 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'secretSantaSite.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
+# Whitenoise for the collectStatic on heroku
+
+# from whitenoise.django import DjangoWhiteNoise
+# application = DjangoWhiteNoise(application)
